eaf27df8d91dec6d6afd974578d55cd1b1aad5a3
Daniel Twardowski <SkiTheSlicer@users.noreply.github.com>
2016-02-10 15:12:13 -0500

    added help menu and option to only download diffs

--- mirror_repo.py
+++ mirror_repo.py
@@ -8,6 +8,22 @@ from collections import OrderedDict
 from math import log
 import subprocess
 
+def parse_arguments():
+  import argparse
+  import os
+  import sys
+  parser = argparse.ArgumentParser(
+    prog='mirror_repo.py',
+    description='Download repository for use on an out-of-band machine.',
+    epilog='Created by SkiTheSlicer (https://github.com/SkiTheSlicer)')
+  parser.add_argument('-u', '--update',
+                      nargs='?',
+                      help='Specifies old Packages.gz to compare; will only download differences.')
+  parser.add_argument('-i', '--iso',
+                      action='store_true',
+                      help='Download and create ISO.')
+  return parser.parse_args()
+
 def requests_download_file(url_to_download, local_dir_name):
   #import requests
   #import os
@@ -29,11 +45,10 @@ def gzip_decompress_file(full_local_path):
   with gzip.open(full_local_path, 'rb') as f:
     return f.read()
 
-def parse_pkg_list(path_repo_pkglist, dir_local_base):
+def parse_pkg_list(local_repo_file):
   #from collections import OrderedDict
   #import os
   #import json
-  local_repo_file = os.path.join(dir_local_base, path_repo_pkglist)
   page_array = gzip_decompress_file(local_repo_file).split("\n\n")
   #packages = {};
   packages = OrderedDict()
@@ -51,7 +66,7 @@ def parse_pkg_list(path_repo_pkglist, dir_local_base):
       packages[package['Package']].update(package)
       #print json.dumps(package, indent=2)
   #print json.dumps(packages, indent=2)
-  with open(os.path.join(dir_local_base, 'Packages.json'), 'w') as outfile:
+  with open(os.path.join(os.path.dirname(local_repo_file), '.'.join(os.path.basename(local_repo_file).split('.')[:-1] + ['json'])), 'w') as outfile:
     json.dump(packages, outfile, indent=2)
   return packages
 
@@ -71,7 +86,7 @@ def sizeof_fmt(num):
   elif num == 1:
     return '1 byte'
 
-def main():
+def define_repo_list():
   #[short name, url base, url dist, url arch]
   repo_list = [
     ["so-14-stable-x64", "http://ppa.launchpad.net/securityonion/stable/ubuntu", "dists/trusty", "main/binary-amd64"],
@@ -80,7 +95,16 @@ def main():
     ["so-12-test-x64", "http://ppa.launchpad.net/securityonion/test/ubuntu", "dists/precise", "main/binary-amd64"],
     ["ubu-14-main-x64", "http://us.archive.ubuntu.com/ubuntu", "dists/trusty", "main/binary-amd64"]
   ]
+  return repo_list
 
+def main():
+  args = parse_arguments()
+  if args.update:
+    if not 'gz' in args.update:
+      import sys
+      print 'Update file doesn\'t end in \'gz\'. Exitting...'
+      sys.exit(1)
+  repo_list = define_repo_list()
   print 'AVAILABLE REPOSITORIES:'
   for idx, entry in enumerate(repo_list):
     print str(idx) + '\t' + entry[0]
@@ -101,29 +125,44 @@ def main():
     print "Folder \"" + dir_local_base + "\" already exists."
 
   requests_download_file(url_repo_pkglist, os.path.join(dir_local_base, dir_repo_arch))
-  for auth_file in ['InRelease', 'Release', 'Release.gpg']:
-    requests_download_file("/".join([url_repo_dist, auth_file]), os.path.join(dir_local_base, dir_repo_dist))
+  if args.iso:
+    for auth_file in ['InRelease', 'Release', 'Release.gpg']:
+      requests_download_file("/".join([url_repo_dist, auth_file]), os.path.join(dir_local_base, dir_repo_dist))
 
-  packages = parse_pkg_list(path_repo_pkglist, dir_local_base)
+  packages = parse_pkg_list(os.path.join(dir_local_base, path_repo_pkglist))
 
-  size = 0
-  for key_name, package in packages.iteritems():
-    size = size + int(package['Size'])
-  print "Expected Repo size: " + sizeof_fmt(size)
+  if not args.update:
+    size = 0
+    for key_name, package in packages.iteritems():
+      size = size + int(package['Size'])
+    print "Expected Repo size: " + sizeof_fmt(size)
 
-  for key_name, package in packages.iteritems():
-    #url_pkg_todownload = "/".join([url_repo_base, package['Filename']])
-    path_pkg_output = os.path.sep.join(package['Filename'].split('/')[:-1])
-    requests_download_file("/".join([url_repo_base, package['Filename']]), os.path.join(dir_local_base, path_pkg_output))
+    for key_name, package in packages.iteritems():
+      #url_pkg_todownload = "/".join([url_repo_base, package['Filename']])
+      path_pkg_output = os.path.sep.join(package['Filename'].split('/')[:-1])
+      requests_download_file("/".join([url_repo_base, package['Filename']]), os.path.join(dir_local_base, path_pkg_output))
+  else:
+    packages_old = parse_pkg_list(args.update)
+    #print '<Package>\n\t<Old Version>\n\t<New Version>'
+    for key_name, package in packages.iteritems():
+      if packages_old[key_name]['Filename'] != package['Filename']:
+        #print key_name + '\n\t' + packages_old[key_name]['Filename'] + '\n\t' + package['Filename']
+        ##url_pkg_todownload = "/".join([url_repo_base, package['Filename']])
+        path_pkg_output = os.path.sep.join(package['Filename'].split('/')[:-1])
+        requests_download_file("/".join([url_repo_base, package['Filename']]), os.path.join(dir_local_base, path_pkg_output))
 
- #Needs check for if host is Linux
-  subprocess.Popen(['mkisofs', '-r', '-J', '-l', '-o', dir_local_base + ".iso", dir_local_base])
+  if args.iso:
+   #Needs check for if host is Linux
+    #subprocess.Popen(['mkisofs', '-r', '-J', '-l', '-o', dir_local_base + ".iso", dir_local_base])
+    subprocess.Popen(['mkisofs', '-r', '-jcharset=utf-8', '-l', '-o', dir_local_base + ".iso", dir_local_base])
 
-  with open(dir_local_base + ".sh", 'wb') as binaryfile:
-    binaryfile.write('#!/bin/bash\n')
-    binaryfile.write('sudo mount -o loop ' + dir_local_base + '.iso /mnt/\n')
-    binaryfile.write('echo "deb file:/mnt/ ' + repo_list[repo_choice][2].split('/')[-1] + " " + repo_list[repo_choice][3].split('/')[0] + '" | sudo tee /etc/apt/sources.list.d/securityonion-cdrom.list\n')
-    binaryfile.write('#sudo apt-get update && sudo apt-get dist-upgrade\n')
-    binaryfile.write('sudo /usr/bin/soup\n')
+    with open(dir_local_base + ".sh", 'wb') as binaryfile:
+      binaryfile.write('#!/bin/bash\n')
+      binaryfile.write('sudo mount -o loop ' + dir_local_base + '.iso /mnt/\n')
+      binaryfile.write('echo "deb file:/mnt/ ' + repo_list[repo_choice][2].split('/')[-1] + " " + repo_list[repo_choice][3].split('/')[0] + '" | sudo tee /etc/apt/sources.list.d/securityonion-cdrom.list\n')
+      binaryfile.write('#sudo apt-get update && sudo apt-get dist-upgrade\n')
+      binaryfile.write('sudo /usr/bin/soup\n')
+      binaryfile.write('sudo umount /mnt/\n')    
 
-main()
+if __name__ == "__main__":
+  main()
